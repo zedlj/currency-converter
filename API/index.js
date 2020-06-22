b152c294cf25de8f2483baeabadd985bdac2b84f
zedlj <zoelauren-jones@hotmail.co.uk>
2020-06-22 15:36:33 +0100

    modified listeners

--- API/index.js
+++ API/index.js
@@ -4,40 +4,37 @@ fetch('https://api.exchangeratesapi.io/latest')
 .then(data => {
   let rates = data.rates;
   for (const [curr, rate] of Object.entries(rates)) {
-    const rate_option = document.getElementById("Select");
+    const rateOption = document.getElementById("Select");
     const option = document.createElement("option");
     option.text = `${curr}`;
-    rate_option.add(option);
+    rateOption.add(option);
     option.setAttribute("value", `${rate}`);
     option.setAttribute("id", `${rate}`);
   }
 }) 
 
-// everything is ok except initial input / click 
-//first key press or click triggers function from HTML, so only registers click or input contents on
-//second input / click
-// need to clear output box to zero when backspaced after last inut character- reset state ?
 
 function clickListener(userInput){  
-document.getElementById("Select").addEventListener('click', function(e) { 
-  let target = e.target, 
+  // console.log('type')
+  document.getElementById("Select").addEventListener("click", function(e) { 
+      let target = e.target, 
       rate = target.value 
       // console.log('click', rate, userInput)  
-        InputListener(rate);  
-      if (userInput && rate > 0){
+      inputListener(rate);  
+      if (userInput && rate >= 0){
         calculateConversion(userInput, rate);
       }
   });
 }
 
 
-function InputListener(rate){
-  document.getElementById("userInput").addEventListener("input", function(e) {
-  let target = e.target, 
-      userInput = target.value 
-      // console.log('type', rate, userInput)  
-      clickListener(userInput);
-    if (userInput && rate > 0){
+function inputListener(rate){
+  document.getElementById("userInput").addEventListener("keyup", function(e) {
+    let target = e.target, 
+    userInput = target.value 
+    // console.log('type', rate, userInput)  
+    clickListener(userInput);
+    if (userInput && rate >= 0){
       calculateConversion(userInput, rate);
     }
   });
@@ -45,9 +42,11 @@ function InputListener(rate){
 
 
 function calculateConversion(userInput, rate){
-  if (userInput && rate > 0) {
     let result = rate*userInput
     document.getElementById("output").value = `${result}`;
-  }
 }
 
+
+
+inputListener();
+clickListener();
